3d0f74c6c91534f47cc6de1221892dc0c3f6ccd8
Incinirate <dogescript@gmail.com>
2019-10-18 22:14:54 -0600

    add new pec opcodes

--- disassembler/opcodes.lua
+++ disassembler/opcodes.lua
@@ -11,129 +11,129 @@ local types = {
 
 
 local limn1kOpcodes = {
-  [0x00] = { mnc = "NOP", op = {} },
+  [0x00] = { mnc = "nop", op = {} },
 
-  [0x01] = { mnc = "LI",  op = { types.reg, types.imm } },
-  [0x02] = { mnc = "MOV", op = { types.reg, types.reg } },
-  [0x03] = { mnc = "XCH", op = { types.reg, types.reg } },
+  [0x01] = { mnc = "li",  op = { types.reg, types.imm } },
+  [0x02] = { mnc = "mov", op = { types.reg, types.reg } },
+  [0x03] = { mnc = "xch", op = { types.reg, types.reg } },
 
-  [0x04] = { mnc = "LRI.B", op = { types.reg, types.imm } },
-  [0x05] = { mnc = "LRI.I", op = { types.reg, types.imm } },
-  [0x06] = { mnc = "LRI.L", op = { types.reg, types.imm } },
+  [0x04] = { mnc = "lri.b", op = { types.reg, types.imm } },
+  [0x05] = { mnc = "lri.i", op = { types.reg, types.imm } },
+  [0x06] = { mnc = "lri.l", op = { types.reg, types.imm } },
 
-  [0x07] = { mnc = "SIR.B", op = { types.imm, types.reg } },
-  [0x08] = { mnc = "SIR.I", op = { types.imm, types.reg } },
-  [0x09] = { mnc = "SIR.L", op = { types.imm, types.reg } },
+  [0x07] = { mnc = "sir.b", op = { types.imm, types.reg } },
+  [0x08] = { mnc = "sir.i", op = { types.imm, types.reg } },
+  [0x09] = { mnc = "sir.l", op = { types.imm, types.reg } },
 
-  [0x0A] = { mnc = "LRR.B", op = { types.reg, types.reg } },
-  [0x0B] = { mnc = "LRR.I", op = { types.reg, types.reg } },
-  [0x0C] = { mnc = "LRR.L", op = { types.reg, types.reg } },
+  [0x0A] = { mnc = "lrr.b", op = { types.reg, types.reg } },
+  [0x0B] = { mnc = "lrr.i", op = { types.reg, types.reg } },
+  [0x0C] = { mnc = "lrr.l", op = { types.reg, types.reg } },
 
-  [0x0D] = { mnc = "SRR.B", op = { types.reg, types.reg } },
-  [0x0E] = { mnc = "SRR.I", op = { types.reg, types.reg } },
-  [0x0F] = { mnc = "SRR.L", op = { types.reg, types.reg } },
+  [0x0D] = { mnc = "srr.b", op = { types.reg, types.reg } },
+  [0x0E] = { mnc = "srr.i", op = { types.reg, types.reg } },
+  [0x0F] = { mnc = "srr.l", op = { types.reg, types.reg } },
 
-  [0x10] = { mnc = "SII.B", op = { types.imm, types.imm_b } },
-  [0x11] = { mnc = "SII.I", op = { types.imm, types.imm_i } },
-  [0x12] = { mnc = "SII.L", op = { types.imm, types.imm } },
+  [0x10] = { mnc = "sii.b", op = { types.imm, types.imm_b } },
+  [0x11] = { mnc = "sii.i", op = { types.imm, types.imm_i } },
+  [0x12] = { mnc = "sii.l", op = { types.imm, types.imm } },
 
-  [0x13] = { mnc = "SRI.B", op = { types.reg, types.imm_b } },
-  [0x14] = { mnc = "SRI.I", op = { types.reg, types.imm_i } },
-  [0x15] = { mnc = "SRI.L", op = { types.reg, types.imm } },
-
-
-  [0x16] = { mnc = "PUSH",  op = { types.reg } },
-  [0x17] = { mnc = "PUSHI", op = { types.imm } },
-  [0x18] = { mnc = "PUSHA", op = {} },
-  [0x19] = { mnc = "POP",   op = { types.reg } },
-  [0x1A] = { mnc = "POPA",  op = {} },
-
-  [0x1B] = { mnc = "B",   op = { types.imm } },
-  [0x1C] = { mnc = "BR",  op = { types.reg } },
-  [0x1D] = { mnc = "BE",  op = { types.imm } },
-  [0x1E] = { mnc = "BNE", op = { types.imm } },
-  [0x1F] = { mnc = "BG",  op = { types.imm } },
-  [0x20] = { mnc = "BL",  op = { types.imm } },
-  [0x21] = { mnc = "BGE", op = { types.imm } },
-  [0x22] = { mnc = "BLE", op = { types.imm } },
-
-  [0x23] = { mnc = "CALL", op = { types.imm } },
-  [0x24] = { mnc = "RET",  op = {} },
-
-  [0x25] = { mnc = "CMP",  op = { types.reg, types.reg } },
-  [0x26] = { mnc = "CMPI", op = { types.reg, types.imm } },
+  [0x13] = { mnc = "sri.b", op = { types.reg, types.imm_b } },
+  [0x14] = { mnc = "sri.i", op = { types.reg, types.imm_i } },
+  [0x15] = { mnc = "sri.l", op = { types.reg, types.imm } },
 
 
-  [0x27] = { mnc = "ADD",  op = { types.reg, types.reg, types.reg } },
-  [0x28] = { mnc = "ADDI", op = { types.reg, types.reg, types.imm } },
+  [0x16] = { mnc = "push",  op = { types.reg } },
+  [0x17] = { mnc = "pushi", op = { types.imm } },
+  [0x18] = { mnc = "pusha", op = {} },
+  [0x19] = { mnc = "pop",   op = { types.reg } },
+  [0x1A] = { mnc = "popa",  op = {} },
 
-  [0x29] = { mnc = "SUB",  op = { types.reg, types.reg, types.reg } },
-  [0x2A] = { mnc = "SUBI", op = { types.reg, types.reg, types.imm } },
+  [0x1B] = { mnc = "b",   op = { types.imm } },
+  [0x1C] = { mnc = "br",  op = { types.reg } },
+  [0x1D] = { mnc = "be",  op = { types.imm } },
+  [0x1E] = { mnc = "bne", op = { types.imm } },
+  [0x1F] = { mnc = "bg",  op = { types.imm } },
+  [0x20] = { mnc = "bl",  op = { types.imm } },
+  [0x21] = { mnc = "bge", op = { types.imm } },
+  [0x22] = { mnc = "ble", op = { types.imm } },
 
-  [0x2B] = { mnc = "MUL",  op = { types.reg, types.reg, types.reg } },
-  [0x2C] = { mnc = "MULI", op = { types.reg, types.reg, types.imm } },
+  [0x23] = { mnc = "call", op = { types.imm } },
+  [0x24] = { mnc = "ret",  op = {} },
 
-  [0x2D] = { mnc = "MUL",  op = { types.reg, types.reg, types.reg } },
-  [0x2E] = { mnc = "MULI", op = { types.reg, types.reg, types.imm } },
-
-  [0x2F] = { mnc = "MOD",  op = { types.reg, types.reg, types.reg } },
-  [0x30] = { mnc = "MODI", op = { types.reg, types.reg, types.imm } },
-
-
-  [0x31] = { mnc = "NOT",   op = { types.reg, types.reg } },
-
-  [0x32] = { mnc = "IOR",   op = { types.reg, types.reg, types.reg } },
-  [0x33] = { mnc = "IORI",  op = { types.reg, types.reg, types.imm } },
-
-  [0x34] = { mnc = "NOR",   op = { types.reg, types.reg, types.reg } },
-  [0x35] = { mnc = "NORI",  op = { types.reg, types.reg, types.imm } },
-
-  [0x36] = { mnc = "EOR",   op = { types.reg, types.reg, types.reg } },
-  [0x37] = { mnc = "EORI",  op = { types.reg, types.reg, types.imm } },
-
-  [0x38] = { mnc = "AND",   op = { types.reg, types.reg, types.reg } },
-  [0x39] = { mnc = "ANDI",  op = { types.reg, types.reg, types.imm } },
-
-  [0x3A] = { mnc = "NAND",  op = { types.reg, types.reg, types.reg } },
-  [0x3B] = { mnc = "NANDI", op = { types.reg, types.reg, types.imm } },
-
-  [0x3C] = { mnc = "LSH",   op = { types.reg, types.reg, types.reg } },
-  [0x3D] = { mnc = "LSHI",  op = { types.reg, types.reg, types.imm_b } },
-
-  [0x3E] = { mnc = "RSH",   op = { types.reg, types.reg, types.reg } },
-  [0x3F] = { mnc = "RSHI",  op = { types.reg, types.reg, types.imm_b } },
-
-  [0x40] = { mnc = "BSET",  op = { types.reg, types.reg, types.reg } },
-  [0x41] = { mnc = "BSETI", op = { types.reg, types.reg, types.imm_b } },
-
-  [0x42] = { mnc = "BCLR",  op = { types.reg, types.reg, types.reg } },
-  [0x43] = { mnc = "BCLRI", op = { types.reg, types.reg, types.imm_b } },
+  [0x25] = { mnc = "cmp",  op = { types.reg, types.reg } },
+  [0x26] = { mnc = "cmpi", op = { types.reg, types.imm } },
 
 
-  [0x44] = { mnc = "SYS",   op = { types.imm } },
+  [0x27] = { mnc = "add",  op = { types.reg, types.reg, types.reg } },
+  [0x28] = { mnc = "addi", op = { types.reg, types.reg, types.imm } },
 
-  [0x45] = { mnc = "CLI",   op = {} },
-  [0x46] = { mnc = "BRK",   op = {} },
-  [0x47] = { mnc = "HLT",   op = {} },
-  [0x48] = { mnc = "IRET",  op = {} },
+  [0x29] = { mnc = "sub",  op = { types.reg, types.reg, types.reg } },
+  [0x2A] = { mnc = "subi", op = { types.reg, types.reg, types.imm } },
 
-  [0x49] = { mnc = "BSWAP", op = { types.reg, types.reg } },
+  [0x2B] = { mnc = "mul",  op = { types.reg, types.reg, types.reg } },
+  [0x2C] = { mnc = "muli", op = { types.reg, types.reg, types.imm } },
 
-  [0x4A] = { mnc = "HTTL",  op = {} },
-  [0x4B] = { mnc = "HTTS",  op = {} },
+  [0x2D] = { mnc = "mul",  op = { types.reg, types.reg, types.reg } },
+  [0x2E] = { mnc = "muli", op = { types.reg, types.reg, types.imm } },
 
-  [0x4C] = { mnc = "CPU",   op = {} },
+  [0x2F] = { mnc = "mod",  op = { types.reg, types.reg, types.reg } },
+  [0x30] = { mnc = "modi", op = { types.reg, types.reg, types.imm } },
 
 
-  [0x4D] = { mnc = "RSP",   op = { types.reg } },
-  [0x4E] = { mnc = "SSP",   op = { types.reg } },
+  [0x31] = { mnc = "not",   op = { types.reg, types.reg } },
 
-  [0x4F] = { mnc = "PUSHV",  op = { types.reg, types.reg } },
-  [0x50] = { mnc = "PUSHVI", op = { types.reg, types.imm } },
-  [0x51] = { mnc = "POPV",   op = { types.reg, types.reg } },
+  [0x32] = { mnc = "ior",   op = { types.reg, types.reg, types.reg } },
+  [0x33] = { mnc = "iori",  op = { types.reg, types.reg, types.imm } },
 
-  [0x52] = { mnc = "CMPS",   op = { types.reg, types.reg } },
-  [0x53] = { mnc = "CMPSI",  op = { types.reg, types.imm } }
+  [0x34] = { mnc = "nor",   op = { types.reg, types.reg, types.reg } },
+  [0x35] = { mnc = "nori",  op = { types.reg, types.reg, types.imm } },
+
+  [0x36] = { mnc = "eor",   op = { types.reg, types.reg, types.reg } },
+  [0x37] = { mnc = "eori",  op = { types.reg, types.reg, types.imm } },
+
+  [0x38] = { mnc = "and",   op = { types.reg, types.reg, types.reg } },
+  [0x39] = { mnc = "andi",  op = { types.reg, types.reg, types.imm } },
+
+  [0x3A] = { mnc = "nand",  op = { types.reg, types.reg, types.reg } },
+  [0x3B] = { mnc = "nandi", op = { types.reg, types.reg, types.imm } },
+
+  [0x3C] = { mnc = "lsh",   op = { types.reg, types.reg, types.reg } },
+  [0x3D] = { mnc = "lshi",  op = { types.reg, types.reg, types.imm_b } },
+
+  [0x3E] = { mnc = "rsh",   op = { types.reg, types.reg, types.reg } },
+  [0x3F] = { mnc = "rshi",  op = { types.reg, types.reg, types.imm_b } },
+
+  [0x40] = { mnc = "bset",  op = { types.reg, types.reg, types.reg } },
+  [0x41] = { mnc = "bseti", op = { types.reg, types.reg, types.imm_b } },
+
+  [0x42] = { mnc = "bclr",  op = { types.reg, types.reg, types.reg } },
+  [0x43] = { mnc = "bclri", op = { types.reg, types.reg, types.imm_b } },
+
+
+  [0x44] = { mnc = "sys",   op = { types.imm } },
+
+  [0x45] = { mnc = "cli",   op = {} },
+  [0x46] = { mnc = "brk",   op = {} },
+  [0x47] = { mnc = "hlt",   op = {} },
+  [0x48] = { mnc = "iret",  op = {} },
+
+  [0x49] = { mnc = "bswap", op = { types.reg, types.reg } },
+
+  [0x4A] = { mnc = "httl",  op = {} },
+  [0x4B] = { mnc = "htts",  op = {} },
+
+  [0x4C] = { mnc = "cpu",   op = {} },
+
+
+  [0x4D] = { mnc = "rsp",   op = { types.reg } },
+  [0x4E] = { mnc = "ssp",   op = { types.reg } },
+
+  [0x4F] = { mnc = "pushv",  op = { types.reg, types.reg } },
+  [0x50] = { mnc = "pushvi", op = { types.reg, types.imm } },
+  [0x51] = { mnc = "popv",   op = { types.reg, types.reg } },
+
+  [0x52] = { mnc = "cmps",   op = { types.reg, types.reg } },
+  [0x53] = { mnc = "cmpsi",  op = { types.reg, types.imm } }
 }
 
 setmetatable(limn1kOpcodes, {
@@ -183,6 +183,15 @@ local pecOpcodes = {
   [0x19] = { mnc = "pushd", op = {} },
 
   [0x1A] = { mnc = "ncall", op = { types.imm } },
+
+  [0x1B] = { mnc = "base",  op = {} },
+
+  [0x1C] = { mnc = "slot",  op = {} },
+
+  [0x1D] = { mnc = "xor",   op = {} },
+  [0x1E] = { mnc = "or",    op = {} },
+  [0x1F] = { mnc = "not",   op = {} },
+  [0x20] = { mnc = "ans",   op = {} },
 }
 
 return {
